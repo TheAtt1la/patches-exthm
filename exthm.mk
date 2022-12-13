6a180c466a1b034a2f00c554b4f2153f8570114a
TheAtt1la <alittauwu@gmail.com>
2022-12-13 00:52:10 -0300

    add

---
+++ exthm.mk
@@ -0,0 +1,16 @@
+$(call inherit-product, vendor/exthm/config/common.mk)
+$(call inherit-product, vendor/exthm/config/BoardConfigExthm.mk)
+$(call inherit-product, vendor/exthm/config/BoardConfigSoong.mk)
+$(call inherit-product, device/exthm/sepolicy/common/sepolicy.mk)
+-include vendor/exthm/build/core/config.mk
+
+TARGET_NO_KERNEL_OVERRIDE := true
+TARGET_NO_KERNEL_IMAGE := true
+
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
+TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
+SELINUX_IGNORE_NEVERALLOWS := true
+
+TARGET_BOOT_ANIMATION_RES := 1080
+#WITH_GMS := true
+
